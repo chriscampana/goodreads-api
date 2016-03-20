3926f15578405268d19e49762d88fa1f82cc0b5d
chriscampana <crcampana@gmail.com>
2016-03-20 16:58:23 -0400

    display currently reading on same page

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+function apiTest() {
+    $response = file_get_contents('https://www.goodreads.com/review/list?v=2&key=HcuvAy1HVWamfQboaYl9g&id=39127415&shelf=currently-reading');
+    $xml=simplexml_load_string($response) or die("Error: Cannot create object");
+    $reviews = $xml->reviews[0]->review;
+
+    foreach($reviews as $reviewlist):
+    	echo $reviewlist->book[0]->title;
+    	echo " - ";
+    	echo $reviewlist->book[0]->authors[0]->author[0]->name;
+		echo "<br>";
+    endforeach;
+
+
+
+}
+
+apiTest();
+?>
+
+
+
