53b35b27152180115beb87c03bd078ff82c98acb
chriscampana <crcampana@gmail.com>
2016-03-21 08:44:38 -0400

    changes

--- functions.php
+++ functions.php
@@ -1,15 +1,27 @@
 <?php
 function apiTest() {
-    $response = file_get_contents('https://www.goodreads.com/review/list?v=2&key=HcuvAy1HVWamfQboaYl9g&id=39127415&shelf=currently-reading');
-    $xml=simplexml_load_string($response) or die("Error: Cannot create object");
+    //$response = file_get_contents('https://www.goodreads.com/review/list?v=2&key=HcuvAy1HVWamfQboaYl9g&id=39127415&shelf=currently-reading');
+    //$xml=simplexml_load_string($response) or die("Error: Cannot create object");
+	$xml=simplexml_load_file('test.xml') or die("Error: Cannot create object");
     $reviews = $xml->reviews[0]->review;
+	//echo $reviews;
 
+
+	echo "<form action=\"hey\">";
+	echo "<select name=\"shelves\">";
     foreach($reviews as $reviewlist):
-    	echo $reviewlist->book[0]->title;
-    	echo " - ";
-    	echo $reviewlist->book[0]->authors[0]->author[0]->name;
-		echo "<br>";
+    	//echo "<li>".$reviewlist->book[0]->authors[0]->author[0]->name."</li>";
+		echo "<option value=\"volvo\">" . $reviewlist->book[0]->authors[0]->author[0]->name ."</option>";
+		
     endforeach;
+	echo "</form>";
+	echo "</select>";
+	echo "<br><br>";
+	echo "<input type=\"submit\">";
+	
+
+
+
 
 
 
---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>GoodReads API Test</title>
+	<link rel="stylesheet" type="text/css" href="main.css"/>
+</head>
+<body>
+
+<h2>Currently Reading:</h2>
+
+<button type="button" onclick="loadDoc()">Get my currently </button>
+<p id="demo1"></p>
+<p id="demo"></p>
+<p id="demo2"></p>
+ 
+ 
+<script>
+function loadDoc() {
+  var xhttp, xmlDoc, txt, x, i;
+  xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+  if (xhttp.readyState == 4 && xhttp.status == 200) {
+	  document.getElementById("demo1").innerHTML = "TrUe";
+    xmlDoc = xhttp.responseText;
+/*
+    x = xmlDoc.getElementsByTagName("title");
+    for (i = 0; i < x.length; i++) {
+      txt = txt + x[i].childNodes[0].nodeValue + "<br>";
+    }
+*/
+	txt = xhttp.statusText;
+    document.getElementById("demo").innerHTML = txt;
+    txt2 = xmlDoc;
+    document.getElementById("demo2").innerHTML = txt2;
+    }
+  else {
+	  document.getElementById("demo1").innerHTML = "FalSE";
+	txt = xhttp.statusText;
+    document.getElementById("demo").innerHTML = txt;  
+  }
+  };
+  xhttp.open("GET", "functions.php", true);
+  xhttp.send();
+}
+</script>
+
+</body>
+</html>
